94ebf6275a0ad2e342ccd9af0c5dbf479d9156c3
Eesaa M <eesaamahmood04@gmail.com>
2025-08-21 17:35:22 +0100

    Creating basic HTML files and rendering with flask

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 from flask import Flask
 from flask import url_for
 from flask import request
+from flask import render_template
 from markupsafe import escape   
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Index Page'
-
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
-
-
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -26,6 +21,13 @@ def show_user_profile(username):
 def health():
     return 'OK'
 
+@app.route('/cars')
+def show_cars():
+    return render_template('cars.html')
+
+@app.route('/add')
+def add_cars():
+    return render_template('add.html')
 
 with app.test_request_context():
     print(url_for('index'))
